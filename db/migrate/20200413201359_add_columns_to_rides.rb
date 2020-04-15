853e775e5b0ff3dca21395f7fce302236eea4783
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-04-15 04:48:34 +0000

    Done.

--- db/migrate/20200413201359_add_columns_to_rides.rb
+++ db/migrate/20200413201359_add_columns_to_rides.rb
@@ -3,4 +3,7 @@ class AddColumnsToRides < ActiveRecord::Migration[5.0]
     add_column :rides, :taxi_id, :integer
     add_column :rides, :passenger_id, :integer
   end
+  
+  add_foreign_key :rides, :taxis
+  add_foreign_key :rides, :passengers
 end
